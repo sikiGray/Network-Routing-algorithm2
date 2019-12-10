31772d3bd9f62a1951cf15db8e0ce4e524cc10f1
Guohongshen <guohongshen2018@outlook.com>
2019-12-10 10:14:32 +0800

    //编写insertEdge.cpp

--- insertEdge.cpp
+++ insertEdge.cpp
@@ -1,1 +1,14 @@
+#include"NetTopo.h"
 
+//带权边插入-函数
+void NetTopo::insertEdge(int v1, int v2, int cost)
+{
+	if (v1 > -1 && v1<maxVertices && v2>-1 && v2 < maxVertices && Edge[v1][v2] == inf)
+	{
+		Edge[v1][v2] = Edge[v2][v1] = cost;
+		numEdges++;
+		return;
+	}
+	else
+		return;
+};
