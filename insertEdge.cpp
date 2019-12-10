557039c189a4d3ff53ef88770bff8619fc77ee82
Guohongshen <guohongshen2018@outlook.com>
2019-12-10 10:23:09 +0800

    注释inserEdge.cpp

--- insertEdge.cpp
+++ insertEdge.cpp
@@ -3,12 +3,12 @@
 //带权边插入-函数
 bool NetTopo::insertEdge(int v1, int v2, int cost)
 {
-	if (v1 > -1 && v1<maxVertices && v2>-1 && v2 < maxVertices && Edge[v1][v2] == inf)
+	if (v1 > -1 && v1<maxVertices && v2>-1 && v2 < maxVertices && Edge[v1][v2] == inf)//检查“v1—v2”的合法性：1、v1是路由集合里的路由集合；2、“v1—v2”边长必须是目前不可达的，如果已经可达就无法修改。
 	{
-		Edge[v1][v2] = Edge[v2][v1] = cost;
-		numEdges++;
-		return true;
+		Edge[v1][v2] = Edge[v2][v1] = cost;//无向图
+		numEdges++;//边的个数增加
+		return true;//修改成功
 	}
 	else
-		return false;
+		return false;//修改失败
 };
