62489c3622879524f108a28917f0e76b6a3ee636
zhao <2963286036@qq.com>
2019-12-16 09:19:34 +0800

    finish printRoutingTable function

--- printRoutingTable.cpp
+++ printRoutingTable.cpp
@@ -1,1 +1,29 @@
+//打印路由表
+void NetTopo::printRoutingTable(int v,int dist[],int path[])
+{
+    /*
+    for(int i=0;i<numVertices;++i)
+    {
+        for(int j=0;j<numVertices;++j)
+            cout<<Edge[i][j]<<' ';
+        cout<<endl;
+    }
+    for(int i=0;i<numVertices;++i)
+        cout<<path[i]<<' ';
+    cout<<endl;*/
 
+    cout<<"路由器"<<VerticesList[v]<<"的路由表如下："<<endl;
+    cout<<"\t目的网络"<<"\t下一跳"<<endl;
+    for(int i=0;i<numVertices;++i)
+    {
+        if(i!=v)
+        {
+            int k=i;
+            while(path[k]!=v)
+            {
+                k=path[k];
+            }
+            cout<<"\t  "<<VerticesList[i]<<"\t\t  "<<VerticesList[k]<<endl;
+        }
+    }
+}
