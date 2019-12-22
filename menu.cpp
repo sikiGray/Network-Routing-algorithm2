f797775556a7a9697fa3c1ac46d1a99558887311
zhao <2963286036@qq.com>
2019-12-22 15:37:20 +0800

    创建菜单函数

---
+++ menu.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void menu()
+{
+    cout<<endl;
+    cout<<"\t\t1、从文件导入网络拓扑信息";
+    cout<<"\t\t2、插入新的顶点"<<endl<<endl;
+
+    cout<<"\t\t3、插入新的边";
+    cout<<"\t\t\t\t4、删除指定顶点"<<endl<<endl;
+
+    cout<<"\t\t5、删除指定边";
+    cout<<"\t\t\t\t6、输出指定路由器的路由表"<<endl<<endl;
+}
